c8d49d24a15c0d9c297203c9f6a0cf01db3db710
404pandas <113853550+404pandas@users.noreply.github.com>
2022-12-05 15:58:35 -0500

    POST for category and tag working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -13,10 +13,10 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = Tag.create(req.body);
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
